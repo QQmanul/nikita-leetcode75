9e2e30fa928c7a00230a1866efc24142fd84116f
Nikita Zworykin <sir.zworikin@yandex.ru>
2025-11-10 20:45:26 +0300

    feature: "Move Zeroes" problem

--- src/test/java/ru/nikita/problem283/MoveZeroesTest.java
+++ src/test/java/ru/nikita/problem283/MoveZeroesTest.java
@@ -3,8 +3,6 @@ package ru.nikita.problem283;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Arrays;
-
 class MoveZeroesTest {
 
     @Test
